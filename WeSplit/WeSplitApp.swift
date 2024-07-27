20403f7676e3fd7c157395e6c72b4560bffd8a01
papapahadi <singhjatin1812@gmail.com>
2024-07-27 15:32:31 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jatin Singh on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
