b42226df2291295bce36286a8720ff82efd0dbed
papapahadi <singhjatin1812@gmail.com>
2024-07-28 20:55:38 +0530

    i was just checking if it works fine

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -40,7 +40,7 @@ struct ContentView: View {
                     
                 }
                 
-                Section("how much do you want to tip"){
+                Section("how much do you want to tip : "){
                     Picker("Tip Percentage", selection: $tipPercentage){
                         ForEach(tipPercentages, id: \.self) {
                             Text($0, format: .percent)
